1e3ee4f516fca23533ae97e2e2b4fddaefba227c
Wojciech Paluch <wojciech.paluch@o2.pl>
2017-09-25 23:20:37 +0200

    added function game

--- game/js/engine.js
+++ game/js/engine.js
@@ -1,14 +1,30 @@
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
 
 canvas.width = 1200;
 canvas.height = 600;
 
-var cw = canvas.width;
-var ch = canvas.height;
+const cw = canvas.width;
+const ch = canvas.height;
 
 function map() {
     ctx.fillStyle = 'darkblue';
-    ctx.fillRect(0,0, cw, ch);
+    ctx.fillRect(0, 0, cw, ch);
 }
-map()
+
+const orbSize = 25;
+let orbX = cw / 2;
+let orbY = ch / 2;
+
+
+function orb() {
+    ctx.fillstyle = '#ffffff';
+    ctx.fillRect(orbX, orbY, orbSize, orbSize);
+}
+
+function game() {
+    map()
+    orb()
+}
+
+game()
