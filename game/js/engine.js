9e281a6659b9ec48e4f4b1ab53c74fb231a9fa29
Wojciech Paluch <wojciech.paluch@o2.pl>
2017-09-26 10:39:07 +0200

    little change in function orb

--- game/js/engine.js
+++ game/js/engine.js
@@ -7,21 +7,22 @@ canvas.height = 600;
 const cw = canvas.width;
 const ch = canvas.height;
 
-function map() {
-    ctx.fillStyle = 'darkblue';
-    ctx.fillRect(0, 0, cw, ch);
-}
 
-const orbSize = 25;
+
+const orbSize = 55;
 let orbX = cw / 2;
 let orbY = ch / 2;
 
 
 function orb() {
-    ctx.fillstyle = '#ffffff';
+    ctx.fillStyle = 'white';
     ctx.fillRect(orbX, orbY, orbSize, orbSize);
 }
 
+function map() {
+    ctx.fillStyle = 'darkblue';
+    ctx.fillRect(0, 0, cw, ch);
+}
 function game() {
     map()
     orb()
